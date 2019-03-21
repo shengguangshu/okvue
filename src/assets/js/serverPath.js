3bc2484fd851955da0cf18c0c29afdf3c15c7527
shengzhizhe <15100162730@163.com>
2019-03-21 10:41:58 +0800

    1

--- src/assets/js/serverPath.js
+++ src/assets/js/serverPath.js
@@ -1,16 +1,5 @@
-// 董嘉琛
-// const serverPath = '192.168.1.209:9002';
-// 丁
-// const serverPath = '192.168.1.66:9002';
-// LD
-// const serverPath = '192.168.1.221:9002'; 
-// 李立立
-// const serverPath = '192.168.1.200:9002';
-// 远程服务器
-// const serverPath = '219.148.38.131:9002';
 // 本地
 const serverPath = 'localhost:9000';
-
 
 export default {
 	serverPath
